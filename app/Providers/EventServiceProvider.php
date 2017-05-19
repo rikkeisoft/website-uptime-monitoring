802c6b89a299225c0f90a932f3e13e598ae1d912
phamvankhanh <khanhpv@rikkeisoft.com>
2017-05-19 15:30:31 +0700

    Deleted file trash

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Event;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -13,9 +12,7 @@ class EventServiceProvider extends ServiceProvider
      * @var array
      */
     protected $listen = [
-        'App\Events\Event' => [
-            'App\Listeners\EventListener',
-        ],
+     
          'App\Events\UserCreated' => [
             'App\Listeners\UserCreatedListener',
         ],
