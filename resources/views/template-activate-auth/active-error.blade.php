b7eb0d0aeabcc4cd8d7679d38872e32f501ac0db
phamvankhanh <khanhpv@rikkeisoft.com>
2017-05-19 16:29:51 +0700

    Update fix style

--- resources/views/template-activate-auth/active-error.blade.php
+++ resources/views/template-activate-auth/active-error.blade.php
@@ -5,6 +5,6 @@
 	<style type="text/css">.container{background: #292222;width: 100%;height: 1000px;} .container>h1{font-weight: bold;color:white;text-align: center; margin:0;padding-top: 20%;}</style>
 </head>
 <body>
-         <div class="container"><h1>Account does not exist or not activate,Please try again!</h1></div>
+         <div class="container"><h1>Account does not exist or not activate.Please try again!</h1></div>
 </body>
 </html>
