e6baad46fbe1a301a573ce4f2805cc285334d721
TranbinhLuanj <luantranptiz@gmail.com>
2017-05-23 14:30:58 +0700

    - update homecontroller

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Repositories\AlertGroupsRepository as AlertGroup;
 
 class HomeController extends Controller
 {
-
-    /**
-     * @var Actor
-     */
-    private $actor;
-
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct(AlertGroup $alertGroup)
+    public function __construct()
     {
         $this->middleware('auth');
-
-        $this->actor = $alertGroup;
     }
 
     /**
@@ -32,10 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $alert = $this->actor->findAllBy('user_id', 'b00e35f0-92e6-4764-80af-b9dfcbff0cbf');
-
-        dd($alert);
-
         return view('home');
     }
 }
