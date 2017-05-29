a039ba787ca0c577c95ffc9fce7708f4b5ce4c63
TranbinhLuanj <luantranptiz@gmail.com>
2017-05-29 14:54:07 +0700

    - update model

--- app/Models/AlertMethodAlertGroup.php
+++ app/Models/AlertMethodAlertGroup.php
@@ -1,14 +1,17 @@
 <?php
 
 namespace App\Models;
+use App\Contracts\DBTable;
 
 class AlertMethodAlertGroup extends BaseModel
 {
-    protected $table ='alert_method_alert_group';
+    protected $table = DBTable::ALERT_METHOD_ALERT_GROUP;
+
     protected $fillable = [
         'alert_method_id',
         'alert_group_id'
     ];
+
     public function alertGroup()
     {
         return $this->belongsTo(AlertGroup::class, 'alert_group_id', 'id');
