d31c4452aa28cd0ea0c3e7d1a8021b50f6a33748
phamvankhanh <khanhpv@rikkeisoft.com>
2017-05-29 16:18:09 +0700

    Update file event send alert group

--- app/Events/SendAlertToGroup.php
+++ app/Events/SendAlertToGroup.php
@@ -5,10 +5,8 @@ namespace App\Events;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Broadcasting\PrivateChannel;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 
 class SendAlertToGroup
 {
