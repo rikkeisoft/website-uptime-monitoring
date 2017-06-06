9524a995ccec1ca06058b0166f18fa4db6f06f25
TranBinhLuan <luantranptiz@gmail.com>
2017-06-06 17:48:28 +0700

    update message after active email (#24)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,6 +7,11 @@ Register
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+            @component('flash_alert_message')
+
+            @endcomponent
+        </div>
+        <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading">Login</div>
                 <div class="panel-body">
