1731425f1fc1c5dd14f74c3920cd552b370aa7b8
TranbinhLuanj <luantranptiz@gmail.com>
2017-06-06 16:54:12 +0700

    update teamplate home wwebsite

---
+++ resources/views/teamplate_footer.php
@@ -0,0 +1,23 @@
+<div id="footer" class="block-short-wrapper">
+    <div id="footerIn" class="block-short-inner container">
+        <div class="footerInL">
+            <div class="footer-add-top">
+                <div class="footerInLLogo">
+                    <a href="http://rikkeisoft.com">
+                        <img src="http://rikkeisoft.com/wp-content/themes/rikkei/images/common/footer_logo.png"
+                             alt="ベトナムオフショア開発 Rikkeisoft" width="120">
+                    </a>
+                </div>
+                <p class="rk-address">
+                    3rd Floor, HH3 Building, Me Tri St., Nam Tu Liem Dist., Hanoi, Vietnam<br>
+                    TEL：(+84) 43-6231-685　FAX：(+84) 43-6231-686<br>
+                </p>
+                <address class="address-general">© Rikkeisoft Co.,Ltd All Rights Reserved.</address>
+            </div>
+
+        </div>
+        <div class="footerInR">
+
+        </div>
+    </div>
+</div>
